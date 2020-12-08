303daeb32d842cdfa35e96d20b519f38a0cdcc5c
Giuseppe Eletto <peppe.eletto@gmail.com>
2020-12-08 13:09:34 +0100

    A.A. in javascipt #1

---
+++ js/aay.js
@@ -0,0 +1,2 @@
+/* https://gist.github.com/giuseppe998e/a3fdec46b1736c0a6602fbdbed84f71e */
+!function(){const e=new Date,t=e.getMonth()>8,n=e.getFullYear()-1*!t,a=e.getFullYear()+1*t;document.getElementById("js-aa").innerText=`${n}/${a}`}()
